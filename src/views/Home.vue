ed32feac9c18f5df6f0516567d4456dbf8dd4dcd
jhwu <1273163614@qq.com>
2020-05-12 08:25:17 +0800

    移动端音乐框架

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <HelloWorld />
+    <van-card
+      num="2"
+      price="2.00"
+      desc="描述信息"
+      title="商品标题"
+      thumb="https://img.yzcdn.cn/vant/ipad.jpeg"
+    />
+    <van-card
+      num="2"
+      tag="标签"
+      price="2.00"
+      desc="描述信息"
+      title="商品标题"
+      thumb="https://img.yzcdn.cn/vant/ipad.jpeg"
+      origin-price="10.00"
+    />
+    <van-card
+      num="2"
+      price="2.00"
+      desc="描述信息"
+      title="商品标题"
+      thumb="https://img.yzcdn.cn/vant/ipad.jpeg"
+    >
+      <template #tags>
+        <van-tag
+          plain
+          type="danger"
+        >标签</van-tag>
+        <van-tag
+          plain
+          type="danger"
+        >标签</van-tag>
+      </template>
+      <template #footer>
+        <van-button size="mini">按钮</van-button>
+        <van-button size="mini">按钮</van-button>
+      </template>
+    </van-card>
   </div>
 </template>
 
